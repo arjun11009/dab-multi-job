57d9292887cebc13d999b62be1fec1cf97a4b002
Rana <Arjun.Rana@bottomline.com>
2026-03-25 15:34:28 +0530

    updated path

--- src/bronze/ingest.py
+++ src/bronze/ingest.py
@@ -1,6 +1,15 @@
-from src.common.reader import create_sample_data
-from src.common.writer import log_output
+import sys
+import os
 
+# Add src folder to path
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from common.reader import create_sample_data
+from common.writer import log_output
+
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.getOrCreate()
 
 df = create_sample_data(spark)
 
--- src/silver/transform.py
+++ src/silver/transform.py
@@ -1,7 +1,16 @@
-from src.common.reader import create_sample_data
-from src.common.validator import validate_not_null
-from src.common.writer import log_output
+import sys
+import os
 
+# Add src folder to path
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from common.reader import create_sample_data
+from common.validator import validate_not_null
+from common.writer import log_output
+
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.getOrCreate()
 
 df = create_sample_data(spark)
 
